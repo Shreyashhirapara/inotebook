c49e3c40dbda432786495187389eeedb04867c10
Shreyashhirapara <hiraparashreyash5@gmail.com>
2024-07-08 18:16:03 +0530

    code is 59 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
-          <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-            </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar/>
+          <div className="container">
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+              </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
