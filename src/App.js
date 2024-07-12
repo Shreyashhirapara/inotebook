f1491932d49b613e70c70020164272c32c383df2
Shreyashhirapara <hiraparashreyash5@gmail.com>
2024-07-12 19:20:38 +0530

    code is 70 completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Home } from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
+                <Route exact path="/Login" element={<Login />} />
+                <Route exact path="/Signup" element={<Signup />} />
               </Routes>
           </div>
         </Router>
